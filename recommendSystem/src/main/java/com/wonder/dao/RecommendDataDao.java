1b7649ceedda53a564855815a4f0bbe7656e7466
wangadmin <805046661@qq.com>
2017-10-13 08:55:16 +0800

    添加方法的备注说明

--- recommendSystem/src/main/java/com/wonder/dao/RecommendDataDao.java
+++ recommendSystem/src/main/java/com/wonder/dao/RecommendDataDao.java
@@ -10,7 +10,18 @@ public interface RecommendDataDao {
 
     public void translate2Kafka(String message);
 
+    /**
+     * 得到推荐的标签
+     * @param userid
+     * @return
+     * @throws JSONException
+     */
     public String resultItem(String userid) throws JSONException;
 
+    /**
+     * 查询用户的详细标签以及得分
+     * @param userid
+     * @return
+     */
     public Document queryAllItem(String userid);
 }
